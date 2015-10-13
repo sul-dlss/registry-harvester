0d5226d43a81b7297c64e8227c1c23bb663ffbad
Joshua Greben <jgreben@stanford.edu>
2015-10-13 11:55:34 -0700

    Updated profiles map

--- classes/GetProfiles.java
+++ classes/GetProfiles.java
@@ -5,25 +5,27 @@ public class GetProfiles {
 
   public static Map <String, String> profiles (Map profiles) throws Exception {
 
-    profiles.put("Affiliate", "MXFEE");
+    profiles.put("Student", "ARTU");
+    profiles.put("Academic Staff", "CNAC");
+    profiles.put("Faculty", "CNF");
+    profiles.put("Staff", "CNS");
+    profiles.put("Academic Staff", "MXAC");
     profiles.put("Affiliate", "MXAS");
-    profiles.put("Student", "HMSU");
-    profiles.put("Student", "REU");
-    profiles.put("Student", "REU-SUM");
-    profiles.put("Student", "HMSG");
+    profiles.put("Staff", "MXD");
+    profiles.put("Faculty", "MXF");
+    profiles.put("Staff", "MXS");
+    profiles.put("Fee Borrower", "MXFEE");
+    profiles.put("Doctoral Student", "RED");
     profiles.put("Graduate Student", "REG");
     profiles.put("Graduate Student", "REG-SUM");
-    profiles.put("Graduate Student", "HMSD");
-    profiles.put("Graduate Student", "RED");
-    profiles.put("Affiliate", "MXS");
+    profiles.put("Student", "REU");
+    profiles.put("Student", "REU-SUM");
+    profiles.put("Sponsored", "HMSU");
+    profiles.put("Graduate Student", "HMSG");
+    profiles.put("Visiting Scholar", "HMSD");
     profiles.put("Staff", "HMSS");
-    profiles.put("Staff", "CNS");
-    profiles.put("Academic Staff", "HMSAC");
-    profiles.put("Affiliate", "MXAC");
-    profiles.put("Affiliate", "CNAC");
+    profiles.put("Visiting Scholar", "HMSAC");
     profiles.put("Faculty", "HMSF");
-    profiles.put("Faculty", "MXF");
-    profiles.put("Faculty", "CNF");
 
     return profiles;
   }
