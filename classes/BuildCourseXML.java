f680dfd325ff01e8fb944c53ea8a5df297eb267d
Joshua Greben <jgreben@stanford.edu>
2016-09-01 09:01:20 -0700

    Keep track of current courses in xml file and ignore it on git

--- classes/BuildCourseXML.java
+++ classes/BuildCourseXML.java
@@ -1,7 +1,10 @@
-import java.io.StringReader;
-import java.io.File;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringReader;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -18,30 +21,61 @@ public class BuildCourseXML {
     + "<!DOCTYPE CourseClass SYSTEM \"http://registry.stanford.edu/xml/courseclass/1.0/CourseClass.dtd\">"
     + "<RegData>";
 
-    String lineNew = "";
+    String currentCourses = "";
+    try {
+      File file = new File("../include/course.xml");
+      if (!file.exists()) {
+        file.createNewFile();
+      }
+      else {
+        BufferedReader reader = new BufferedReader(new FileReader (file));
+        String fileLine;
+        StringBuilder sb = new StringBuilder();
 
-    for (int f=0; f < args.length; f++) {
-      BufferedReader br = new BufferedReader(new FileReader(new File (args[f])));
-      String line = "";
+        try {
+          while((fileLine = reader.readLine()) != null) {
+            sb.append(fileLine);
+          }
+          currentCourses = sb.toString();
+        } finally {
+          reader.close();
+        }
+      }
 
-      try {   // Only take lines without the xml declarations
+      for (int f=0; f < args.length; f++) {
+        BufferedReader br = new BufferedReader(new FileReader(new File (args[f])));
+        String line = "";
+
+        // Only take lines without the xml declarations
         while ((line = br.readLine()) != null) {
           if (line.indexOf("<?xml") == 0) {
             continue;
           }
           else if (line.indexOf("<?xml") > 0) {
             int idx = line.indexOf("<?xml");
-            lineNew += line.substring(0, idx);
+            String lineNew = line.substring(0, idx);
+
+            // Only save the new courses
+            if (currentCourses.indexOf(lineNew) < 0) {
+              currentCourses += lineNew;
+            }
           }
         }
       }
-      catch(Exception e) {
-        e.printStackTrace();
-      }
-    }
 
-    String xmlBody = declaration + lineNew + "</RegData>";
-    doc = builder.build(new StringReader(xmlBody));
-    CourseTrans.courseDoc(doc);
+      // Overwrite the courses.xml file with the newest courses
+      // and send them to be transformed
+      FileWriter fw = new FileWriter(file);
+      BufferedWriter bw = new BufferedWriter(fw);
+      bw.write(currentCourses);
+      bw.close();
+
+      String xmlBody = declaration + currentCourses + "</RegData>";
+      doc = builder.build(new StringReader(xmlBody));
+      CourseTrans.courseDoc(doc);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
