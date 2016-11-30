97cbaaa9fbdb6e7bc679050fb1540bf6eff2ab36
Joshua Greben <jgreben@stanford.edu>
2016-11-30 10:22:52 -0800

    Create a script to run the course harvest against a file of course IDs (and update script usage wording)

--- run/course_fileLoad.sh
+++ run/course_fileLoad.sh
@@ -1,9 +1,4 @@
-#!/bin/sh
-#$Id: fileLoad.sh,v 1.2 2008/07/22 19:43:28 dtayl Exp $
-#
-#
-# to do:
-#
+/#!/bin/sh
 
 APP_HOME=/s/SUL/Harvester
 JAVA_HOME=/usr
@@ -17,18 +12,17 @@ export LD_LIBRARY_PATH
 # looping to build classpath. skipping anything named old.*.jar
 #
 #
-#echo "building classpath"
+echo "$t_stamp harness building classpath" >> $HARNESS_LOG
 for file in `ls $APP_HOME/jar/` ; do
  case "$file" in
-  old.*.jar) #echo skipping $file
-  ;;
-  *.jar|*.zip) #echo ADDING $file
+  old.*.jar) echo skipping $file >>$HARNESS_LOG;;
+  *.jar|*.zip) echo ADDING $file >> $HARNESS_LOG
         if [ "$CLASSPATH" != "" ]; then
            CLASSPATH=${CLASSPATH}:$APP_HOME/jar/$file
         else
            CLASSPATH=$APP_HOME/jar/$file
         fi
-  ;;
+        ;;
  esac
 done
 
@@ -37,9 +31,10 @@ done
 # well as the name of its property file be specified as a command-line argument
 # -Dlog4j.configuration=<property file>
 #
-CLASSPATH="/home/harvester/WebLogic_lib/wlthint3client-12.2.1.jar":${CLASSPATH}:$APP_HOME/conf
+echo "ADDING wlthint3client-12.2.1.jar" >> $HARNESS_LOG
+CLASSPATH="/s/SUL/Harvester/WebLogic_lib/wlthint3client-12.2.1.jar":${CLASSPATH}:$APP_HOME/conf
 
-$JAVA_HOME/bin/java -Djava.security.egd=file:///dev/urandom -Dlog4j.configuration=harvester.properties -cp $CLASSPATH edu.stanford.harvester.Harvester $APP_HOME/conf/harvester.properties $APP_HOME/conf/processor.properties $LOAD_FILE
+$JAVA_HOME/bin/java -Djava.security.egd=file:///dev/urandom -Dlog4j.configuration=course_harvester.properties -cp $CLASSPATH edu.stanford.harvester.Harvester $APP_HOME/conf/course_harvester.properties $APP_HOME/conf/course_processor.properties $LOAD_FILE
 EXIT_CODE=$?
 
 if [ $EXIT_CODE -gt 0 ] ; then
