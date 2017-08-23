89392885aea8753e77abdd04311464c0275bc924
Joshua Greben <jgreben@stanford.edu>
2017-08-23 10:56:02 -0700

    Makes method for this_quarter and downcase within methods

--- run/update_terms_conf.rb
+++ run/update_terms_conf.rb
@@ -14,22 +14,30 @@ def current_term_hash(date)
   end
 end
 
+def this_quarter(date)
+  terms.each_with_index do |term, ix|
+    return terms[ix][:quarter].downcase if term[:end_date] == date
+  end
+end
+
 def next_quarter(date)
   terms.each_with_index do |term, ix|
-    return terms[ix+1][:quarter] if term[:end_date] == date
+    return terms[ix+1][:quarter].downcase if term[:end_date] == date
   end
 end
 
 def second_next_quarter(date)
   terms.each_with_index do |term, ix|
-    return terms[ix+2][:quarter] if term[:end_date] == date
+    return terms[ix+2][:quarter].downcase if term[:end_date] == date
   end
 end
 
 puts "\nUpdating terms.conf with current terms"
-thisq = terms.select{ |hash| hash[:end_date] == current_term_hash(date) }[0][:quarter]
-nextq = next_quarter(current_term_hash(date)).downcase
-next2q = second_next_quarter(current_term_hash(date)).downcase
+
+thisq = this_quarter(current_term_hash(date))
+nextq = next_quarter(current_term_hash(date))
+next2q = second_next_quarter(current_term_hash(date))
+
 File.open('/s/SUL/Harvester/conf/terms.conf', 'w') do |terms_file|
   puts "TERMS=#{thisq},#{nextq},#{next2q}"
   terms_file.syswrite("TERMS=#{thisq},#{nextq},#{next2q}\n")
