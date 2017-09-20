1f11d55c105b634e834905d9a80d9a3cebcdf433
Joshua Greben <jgreben@stanford.edu>
2017-09-20 13:10:40 -0700

    We do not need a repo_configs linked dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/s/SUL/Harvester-dev"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-set :linked_dirs, ['CourseBuild/src/main/resources', 'PersonToILLiad/src/main/resources', 'etc', 'repo_configs']
+set :linked_dirs, ['CourseBuild/src/main/resources', 'PersonToILLiad/src/main/resources', 'etc']
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
