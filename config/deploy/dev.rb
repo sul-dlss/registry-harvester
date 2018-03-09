01096c87dd363bf9e688281992a7b4623cf9ba73
Joshua Greben <jgreben@stanford.edu>
2018-03-08 16:37:29 -0800

    Changes server name for dev deploys

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,4 @@
-server "morison.stanford.edu", user: "#{fetch(:user)}", roles: %w{app db web}
+server "symphony-app-dev-1.stanford.edu", user: "#{fetch(:user)}", roles: %w{app db web}
 
 # allow ssh to host
 Capistrano::OneTimeKey.generate_one_time_key!
