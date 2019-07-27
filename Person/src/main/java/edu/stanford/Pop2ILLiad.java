096edefe5efdea3280356a56dc36b8dbb03c56f8
Joshua Greben <jgreben@stanford.edu>
2019-07-26 18:15:00 -0700

    Fix email pattern check to account for hyphens in some cases

--- Person/src/main/java/edu/stanford/Pop2ILLiad.java
+++ Person/src/main/java/edu/stanford/Pop2ILLiad.java
@@ -12,7 +12,8 @@ public class Pop2ILLiad {
 
     private static final String EMAIL_PATTERN =
     "^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@"
-    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
 
     public static void main (String [] args) throws Exception {
 
