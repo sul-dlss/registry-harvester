f84ece1ff36db6d47d8f0586d151bf4a0c4a4109
Shelley Doljack <sdoljack@stanford.edu>
2019-11-11 12:52:38 -0800

    Abstracts library patron transformation and adds display name test.

--- Person/src/test/edu.stanford/LibraryPatronTest.java
+++ Person/src/test/edu.stanford/LibraryPatronTest.java
@@ -1,9 +1,8 @@
 package edu.stanford;
 
 import org.junit.Test;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
+
+import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
@@ -11,21 +10,11 @@ import java.io.*;
 import static org.junit.Assert.assertEquals;
 
 public class LibraryPatronTest {
+    public File xslt_file = new File(getClass().getClassLoader().getResource("library_patron.xsl").getFile());
 
     @Test
     public void hasActiveIDs() throws Exception {
-        TransformerFactory factory = TransformerFactory.newInstance();
-        File xslt_file = new File(getClass().getClassLoader().getResource("library_patron.xsl").getFile());
-        Source xslt = new StreamSource(xslt_file);
-        Transformer transformer = factory.newTransformer(xslt);
-
-        File patron_file = new File(this.getClass().getResource("/patron_one.xml").getFile());
-        Source text = new StreamSource(patron_file);
-        StringWriter outWriter = new StringWriter();
-        transformer.transform(text, new StreamResult(outWriter));
-        StringBuffer sb = outWriter.getBuffer();
-        String symphony_user_rec = sb.toString();
-
+        String symphony_user_rec = symphonyUserRecord(new File(this.getClass().getResource("/patron_one.xml").getFile()));
         File fixture_record = new File(this.getClass().getResource("/symph_user_one").getFile());
         String filePath = fixture_record.getPath();
         assertEquals(symphony_user_rec, fileToString(filePath));
@@ -33,21 +22,39 @@ public class LibraryPatronTest {
 
     @Test
     public void hasBlankActiveIDs() throws Exception {
-        TransformerFactory factory = TransformerFactory.newInstance();
-        File xslt_file = new File(getClass().getClassLoader().getResource("library_patron.xsl").getFile());
-        Source xslt = new StreamSource(xslt_file);
-        Transformer transformer = factory.newTransformer(xslt);
-
-        File patron_file = new File(this.getClass().getResource("/patron_two.xml").getFile());
-        Source text = new StreamSource(patron_file);
-        StringWriter outWriter = new StringWriter();
-        transformer.transform(text, new StreamResult(outWriter));
-        StringBuffer sb = outWriter.getBuffer();
-        String symphony_user_rec = sb.toString();
-
+        String symphony_user_rec = symphonyUserRecord(new File(this.getClass().getResource("/patron_two.xml").getFile()));
         File fixture_record = new File(this.getClass().getResource("/symph_user_two").getFile());
         String filePath = fixture_record.getPath();
         assertEquals(symphony_user_rec, fileToString(filePath));
+    }
+
+    @Test
+    public void hasDisplayName() throws Exception {
+        String symphony_user_rec = symphonyUserRecord(new File(this.getClass().getResource("/patron_three.xml").getFile()));
+        File fixture_record = new File(this.getClass().getResource("/symph_user_three").getFile());
+        String filePath = fixture_record.getPath();
+        assertEquals(symphony_user_rec, fileToString(filePath));
+    }
+
+    public Transformer libraryPatronTransformer(File xslt_file) {
+        TransformerFactory factory = TransformerFactory.newInstance();
+
+        Source xslt = new StreamSource(xslt_file);
+        Transformer transformer = null;
+        try {
+            transformer = factory.newTransformer(xslt);
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        }
+        return transformer;
+    }
+
+    public String symphonyUserRecord(File personXmlFile) throws IOException, TransformerException {
+        Source text = new StreamSource(personXmlFile);
+        StringWriter outWriter = new StringWriter();
+        libraryPatronTransformer(xslt_file).transform(text, new StreamResult(outWriter));
+        StringBuffer sb = outWriter.getBuffer();
+        return sb.toString();
     }
 
     public String fileToString(String filePath) throws IOException {
