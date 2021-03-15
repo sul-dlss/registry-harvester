a4476074dc658b99319b4a2e4771ee5806bbad74
Joshua Greben <jgreben@stanford.edu>
2021-03-15 15:17:32 -0700

    Do not use /bin/ruby

--- run/do-person-harvest.sh
+++ run/do-person-harvest.sh
@@ -33,7 +33,7 @@ perl -p -i -e "s/<!DOCTYPE Person SYSTEM \"http:\/\/registry${UAT}.stanford.edu\
 sed -i '/^$/d' $OUT/harvest.xml.out
 
 # Run harvest.xml.out through folio_api_client ruby script
-# /bin/ruby $FOLIO/folio_user.rb $OUT/harvest.xml.out > $LOG/folio.log
+# ruby $FOLIO/folio_user.rb $OUT/harvest.xml.out > $LOG/folio.log
 
 # Generate the flat file for Symphony
 java -cp $HOME/lib/Person-jar-with-dependencies.jar edu.stanford.LibraryPatron $OUT/harvest.xml.out $XSLT/library_patron.xsl > $OUT/harvest.out
