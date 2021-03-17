d4aeffb4af6d9c58b921db8841441dcb660ebd1c
Joshua Greben <jgreben@stanford.edu>
2021-03-17 12:49:52 -0700

    CD into folio_api_client dir to run ruby script

--- run/do-person-harvest.sh
+++ run/do-person-harvest.sh
@@ -2,11 +2,13 @@
 
 . /s/SUL/Config/sirsi.env
 
+[[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
 HOME=/s/SUL/Harvester/current
 LOG=$HOME/log
 OUT=$HOME/out
 XSLT=$HOME/xslt
-FOLIO=/s/SUL/Bin/folio_api_client/current/lib
+FOLIO=/s/SUL/Bin/folio_api_client/current
 KEYS=$2
 DATE=$3
 
@@ -32,9 +34,6 @@ perl -p -i -e 's/\r//g' $LOG/harvest.log
 perl -p -i -e "s/<!DOCTYPE Person SYSTEM \"http:\/\/registry${UAT}.stanford.edu\/xml\/person\/1.2\/Person.dtd\">//g" $OUT/harvest.xml.out
 sed -i '/^$/d' $OUT/harvest.xml.out
 
-# Run harvest.xml.out through folio_api_client ruby script
-ruby $FOLIO/folio_user.rb $OUT/harvest.xml.out > $LOG/folio.log
-
 # Generate the flat file for Symphony
 java -cp $HOME/lib/Person-jar-with-dependencies.jar edu.stanford.LibraryPatron $OUT/harvest.xml.out $XSLT/library_patron.xsl > $OUT/harvest.out
 
@@ -45,6 +44,10 @@ $HOME/run/usertrans.sh $DATE
 illiad_date=`/s/sirsi/Unicorn/Bin/transdate -d-1`
 echo "Updating/Inserting keys from /s/SUL/Batchlog/userload.keys.$illiad_date into ILLiad" >> $LOG/harvest.log
 $HOME/run/pop2illiad.sh $illiad_date
+
+# Run harvest.xml.out through folio_api_client ruby script to load users into FOLIO
+cd $FOLIO/lib
+ruby folio_user.rb $OUT/harvest.xml.out > $LOG/folio.log 2>&1
 
 # Email and move/reset work files
 cat $LOG/harvest.log | mailx -s 'Harvest Log' sul-unicorn-devs@lists.stanford.edu
