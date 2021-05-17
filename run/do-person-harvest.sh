2aa12ddcf088432b5d89aeff4fadb5d97619832f
Joshua Greben <jgreben@stanford.edu>
2021-05-17 12:38:24 -0700

    Maps harvest xml file into temporary arrays for processing in batches

--- run/do-person-harvest.sh
+++ run/do-person-harvest.sh
@@ -2,10 +2,13 @@
 
 . /s/SUL/Config/sirsi.env
 
+[[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
 HOME=/s/SUL/Harvester/current
 LOG=$HOME/log
 OUT=$HOME/out
 XSLT=$HOME/xslt
+FOLIO=/s/SUL/Bin/folio_api_client/current
 KEYS=$2
 DATE=$3
 
@@ -31,9 +34,6 @@ perl -p -i -e 's/\r//g' $LOG/harvest.log
 perl -p -i -e "s/<!DOCTYPE Person SYSTEM \"http:\/\/registry${UAT}.stanford.edu\/xml\/person\/1.2\/Person.dtd\">//g" $OUT/harvest.xml.out
 sed -i '/^$/d' $OUT/harvest.xml.out
 
-# Run harvest.xml.out through folio_user_load ruby script
-#ruby $HOME/folio_user_load/folio_user.rb $OUT/harvest.xml.out > $LOG/folio.log
-
 # Generate the flat file for Symphony
 java -cp $HOME/lib/Person-jar-with-dependencies.jar edu.stanford.LibraryPatron $OUT/harvest.xml.out $XSLT/library_patron.xsl > $OUT/harvest.out
 
@@ -45,8 +45,19 @@ illiad_date=`/s/sirsi/Unicorn/Bin/transdate -d-1`
 echo "Updating/Inserting keys from /s/SUL/Batchlog/userload.keys.$illiad_date into ILLiad" >> $LOG/harvest.log
 $HOME/run/pop2illiad.sh $illiad_date
 
+# Run harvest.xml.out through folio_api_client ruby script to load users into FOLIO
+cd $FOLIO/lib
+# Split into batches of 5000
+while mapfile -t -n 5000 array && ((${#array[@]}))
+do
+    printf '%s\n' "${array[@]}" > tmp.xml
+    ruby folio_user.rb $OUT/tmp.xml >> $LOG/folio.log 2>&1
+    rm tmp.xml
+done < $OUT/harvest.xml.out
+
 # Email and move/reset work files
 cat $LOG/harvest.log | mailx -s 'Harvest Log' sul-unicorn-devs@lists.stanford.edu
+cat $LOG/folio.log | mailx -s 'Folio User Load' sul-unicorn-devs@lists.stanford.edu
 
 # Save output files
 mv $OUT/harvest.out $OUT/harvest.out.$DATE
@@ -55,11 +66,11 @@ mv $OUT/harvest.xml.out $OUT/harvest.xml.out.$DATE
 # Save and reset log files
 mv $LOG/harvest.log $LOG/harvest.log.$DATE
 mv $LOG/illiad.log $LOG/illiad.log.$illiad_date.$DATE
-#mv $LOG/folio.log $LOG/folio.log.$DATE
+mv $LOG/folio.log $LOG/folio.log.$DATE
 
 touch $LOG/harvest.log
 touch $LOG/illiad.log
-#touch $LOG/folio.log
+touch $LOG/folio.log
 
 usage(){
     echo "Usage: $0 [ no argument | 'file' ] [ file of user keys (if arg0 == file) ] [ DATE (optional: to append to log and out files) ]"
