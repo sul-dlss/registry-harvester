ecd37dbfebe3de42b307649a5e96566cd29a5135
Joshua Greben <jgreben@stanford.edu>
2022-02-03 10:32:49 -0800

    Add weblogic upgrade option to all scripts

--- run/course_runonce.sh
+++ run/course_runonce.sh
@@ -55,6 +55,20 @@ for file in `ls $APP_HOME/WebLogic_lib/` ; do
         ;;
  esac
 done
+
+# Weblogic jar file
+for file in `ls $APP_HOME/WebLogic_lib/` ; do
+ case "$file" in
+  old.*.jar) echo skipping $file >>$HARNESS_LOG;;
+  *.jar|*.zip) echo ADDING $file >> $HARNESS_LOG
+        if [ "$CLASSPATH" != "" ]; then
+           CLASSPATH=${CLASSPATH}:$APP_HOME/WebLogic_lib/$file
+        else
+           CLASSPATH=$APP_HOME/WebLogic_lib/$file
+        fi
+        ;;
+ esac
+done
 #
 # Log4j requires that its property file be specified in the CLASSPATH as
 # well as the name of its property file be specified as a command-line argument
--- run/person_file_load.sh
+++ run/person_file_load.sh
@@ -33,12 +33,25 @@ for file in `ls $APP_HOME/jar/` ; do
  esac
 done
 
+# Weblogic jar file
+for file in `ls $APP_HOME/WebLogic_lib/` ; do
+ case "$file" in
+  old.*.jar) echo skipping $file >>$HARNESS_LOG;;
+  *.jar|*.zip) echo ADDING $file >> $HARNESS_LOG
+        if [ "$CLASSPATH" != "" ]; then
+           CLASSPATH=${CLASSPATH}:$APP_HOME/WebLogic_lib/$file
+        else
+           CLASSPATH=$APP_HOME/WebLogic_lib/$file
+        fi
+        ;;
+ esac
+done
 #
 # Log4j requires that its property file be specified in the CLASSPATH as
 # well as the name of its property file be specified as a command-line argument
 # -Dlog4j.configuration=<property file>
 #
-CLASSPATH="/home/harvester/WebLogic_lib/wlthint3client-12.2.1.jar":${CLASSPATH}:$CONF_HOME
+CLASSPATH=${CLASSPATH}:$CONF_HOME
 
 $JAVA_HOME/bin/java -Djava.security.egd=file:///dev/urandom -Dlog4j.configuration=harvester.properties -Dhttps.protocols=TLSv1.2 -cp $CLASSPATH edu.stanford.harvester.Harvester $CONF_HOME/harvester.properties $CONF_HOME/processor.properties $LOAD_FILE
 EXIT_CODE=$?
